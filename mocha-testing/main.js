7f6a1f81580211105bc4cbeae1d7152ffa4faa08
davik98 <davik_98@yahoo.fr>
2026-01-28 18:58:06 +0100

    adding two modules about promises

--- mocha-testing/main.js
+++ mocha-testing/main.js
@@ -1,33 +1,32 @@
-console.log = function () { };
 
-const assert = require('chai').assert;
-const fs = require('fs');
-const Structured = require('structured');
 
-const code = fs.readFileSync('./test/index_test.js', 'utf8');
 
-describe('Checkpoint 2', () => {
-    it('asserts that 3 + 4 == 8', () => {
+// Define a rooster
+Rooster = {};
 
-        let structure = function () {
-            describe(_, () => {
-                it(_, () => {
-                    assert.ok($expr);
-                });
-            });
-        };
+// Return a morning rooster call
+Rooster.announceDawn = () => {
+    return 'moo!';
+}
 
-        // assert that structure matches
-        let isMatch = Structured.match(code, structure);
-        let failureMessage = 'Call `assert.ok(3 + 4 === 8)` in the `it` block.';
-        assert.isOk(isMatch, failureMessage);
+// Return hour as string
+// Throws Error if hour is not between 0 and 23 inclusive
+Rooster.timeAtDawn = (hour) => {
+    if (hour < 0 || hour > 23) {
+        throw new RangeError;
+    } else {
+        return hour.toString();
+    };
+}
 
-        // assert that regex matches
-        let codeMatch = code.match(/assert\.\s*ok\s*\((\d)\s*\+\s*(\d)\s*===\s*8\s*\)/);
-        assert.isOk(codeMatch, 'Use the expression `3 + 4 === 8`.');
+Phrase = {
+    initials(phr) {
+        return 'NM';
+    }
+};
 
-        // assert that first and second capture groups are 3 and 4 or 4 and 3
-        assert.isOk((codeMatch[1] == 3 && codeMatch[2] == 4 || codeMatch[1] == 4 && codeMatch[2] == 3),
-            'Use the expression `3 + 4 === 8`.');
-    });
-});
+
+// module.exports = Rooster;
+module.exports = {
+    Rooster, Phrase
+};
--- mocha-testing/test/main_test.js
+++ mocha-testing/test/main_test.js
@@ -1,13 +1,61 @@
-// import assert here
-const assert = require("assert");
+// RUN > npm test
 
-describe("+", () => {
-    it("returns the sum of its arguments", () => {
-        // Write assertion here
-        assert.ok(3 + 4 === 8);
+const assert = require('assert');
+const { Rooster, Phrase } = require('../main.js');
+
+describe('Phrase', () => {
+    describe('.initials', () => {
+        it('returns the first letter of each word in a phrase.', () => {
+            // Setup
+            const inputName = 'Nelson Mandela';
+            const expectedInitials = 'NM';
+            // Exercise
+            const result = Phrase.initials(inputName);
+            // Verification
+            assert.strictEqual(result, expectedInitials);
+        });
     });
-    it("returns the sum of its arguments version 2", () => {
-        // will pass the test :
-        assert.ok(3 + 4 === 7);
+});
+
+describe('Rooster', () => {
+    describe('.announceDawn', () => {
+        it('returns a rooster call', () => {
+            // setup 
+            const expected = 'moo!'
+            // excercise
+            const res = Rooster.announceDawn();
+            // verify
+            assert.ok(expected === res);
+        });
     });
+    describe('.timeAtDawn', () => {
+        it('returns its argument as a string', () => {
+            //setup 
+            // excercise
+            const res = Rooster.timeAtDawn(12);
+            // verify
+            assert.ok(typeof res === 'string');
+        });
+        it('throws an error if passed a number less than 0', () => {
+            // setup
+            const hour = -1;
+            // exercise 
+            // verify
+            assert.throws(() => {
+                Rooster.timeAtDawn(hour);
+            }, RangeError);
+
+        });
+        it('throws an error if passed a number greater than 23', () => {
+            // setup
+            const hour = 24;
+            // exercise 
+            // verify
+            assert.throws(() => {
+                Rooster.timeAtDawn(hour);
+            }, RangeError);
+
+        });
+    });
+
 });
